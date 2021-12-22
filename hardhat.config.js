208d5ac11be8fc015d2c6e405eb75107f49d8ffd
HODL Engineer <hodl.valley.engineer@gmail.com>
2021-12-22 19:23:24 +0530

    Included hardhat setup

---
+++ hardhat.config.js
@@ -0,0 +1,188 @@
+// hardhat.config.js
+
+require("dotenv/config");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-solhint");
+require("@tenderly/hardhat-tenderly");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-abi-exporter");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+require("hardhat-gas-reporter");
+require("hardhat-spdx-license-identifier");
+require("hardhat-typechain");
+require("hardhat-watcher");
+require("solidity-coverage");
+
+const {removeConsoleLog} = require("hardhat-preprocessor");
+const accounts = [process.env.DEPLOYER_PRIVATE_KEY];
+
+module.exports = {
+  abiExporter: {
+    path: "./abi",
+    clear: false,
+    flat: true,
+  },
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    currency: "USD",
+    enabled: process.env.REPORT_GAS === "true",
+    excludeContracts: ["contracts/mocks/", "contracts/libraries/"],
+  },
+  mocha: {
+    timeout: 20000,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      gasPrice: 120 * 1000000000,
+      chainId: 1,
+    },
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
+    },
+    hardhat: {
+      forking: {
+        enabled: process.env.FORKING === "true",
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+      live: false,
+      saveDeployments: true,
+      tags: ["test", "local"],
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 3,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 5000000000,
+      gasMultiplier: 2,
+    },
+    fantom: {
+      url: "https://rpcapi.fantom.network",
+      accounts,
+      chainId: 250,
+      live: true,
+      saveDeployments: true,
+      gasPrice: 22000000000,
+    },
+    "fantom-testnet": {
+      url: "https://rpc.testnet.fantom.network",
+      accounts,
+      chainId: 4002,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    matic: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts,
+      chainId: 137,
+      live: true,
+      saveDeployments: true,
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts,
+      chainId: 80001,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org",
+      accounts,
+      chainId: 56,
+      live: true,
+      saveDeployments: true,
+    },
+    "bsc-testnet": {
+      url: "https://data-seed-prebsc-2-s3.binance.org:8545",
+      accounts,
+      chainId: 97,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    avalanche: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      accounts,
+      chainId: 43114,
+      live: true,
+      saveDeployments: true,
+      gasPrice: 470000000000,
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts,
+      chainId: 43113,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+  },
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    deploy: "deploy",
+    deployments: "deployments",
+    imports: "imports",
+    sources: "contracts",
+    tests: "test",
+  },
+  preprocess: {
+    eachLine: removeConsoleLog(
+      (bre) =>
+        bre.network.name !== "hardhat" && bre.network.name !== "localhost"
+    ),
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  spdxLicenseIdentifier: {
+    overwrite: false,
+    runOnCompile: true,
+  },
+  tenderly: {
+    project: process.env.TENDERLY_PROJECT,
+    username: process.env.TENDERLY_USERNAME,
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
+  },
+  watcher: {
+    compile: {
+      tasks: ["compile"],
+      files: ["./contracts"],
+      verbose: true,
+    },
+  },
+};
