4b4af3a0a8937976461943698fbd06a70aa6ec60
HODL Engineer <hodl.verse.engineer@gmail.com>
2022-02-09 01:54:52 +0530

    Included governance contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -164,6 +164,15 @@ module.exports = {
           },
         },
       },
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
     ],
   },
   spdxLicenseIdentifier: {
