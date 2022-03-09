6d1e31fbd3c7c39ce5996c7144bba615e1d023c7
Hodl Engineer <hodl.engineer@gmail.com>
2022-03-09 12:23:09 +0100

    Migrator test

--- test/pair/Migrator.test.js
+++ test/pair/Migrator.test.js
@@ -1,6 +1,7 @@
 const { use, expect } = require("chai");
 const { ethers, waffle } = require("hardhat");
 const { solidity } = waffle;
+const loadFixture = require("./fixtures");
 use(solidity);
 
 describe("Migrator", function () {
@@ -9,84 +10,85 @@ describe("Migrator", function () {
     this.owner = this.signers[0];
     this.bob = this.signers[1];
     this.dev = this.signers[2];
-    this.minter = this.signers[3];
+    this.minter = this.signers[0];
 
-    this.Router = await ethers.getContractFactory("Router");
-    this.Factory = await ethers.getContractFactory("Factory");
+
     this.Core = await ethers.getContractFactory("Core");
-    this.ERC20Mock = await ethers.getContractFactory("ERC20Mock", this.minter);
-    this.MoneyToken = await ethers.getContractFactory("MoneyToken");
     this.Migrator = await ethers.getContractFactory("Migrator");
-    this.Buyback = await ethers.getContractFactory("Buyback");
+    this.ERC20Mock = await ethers.getContractFactory("ERC20Mock", this.minter);
   });
 
   beforeEach(async function () {
-    this.money = await this.MoneyToken.deploy();
-    await this.money.deployed();
-    await this.money.__MoneyToken_init();
+    const {
+      token0,
+      money,
+      factory: factory1,
+      router: router1,
+      weth9,
+    } = await loadFixture();
+
+    const {
+      factory: factory2,
+      router: router2,
+      buyback,
+    } = await loadFixture();
+
+    this.money = money
+
 
     this.weth = await this.ERC20Mock.deploy("WETH", "WETH", "100000000");
     await this.weth.deployed();
 
-    this.factory1 = await this.Factory.deploy(this.money.address);
-    await this.factory1.deployed();
 
-    this.factory2 = await this.Factory.deploy(this.money.address);
-    await this.factory2.deployed();
+    this.factory1 = factory1
 
-    this.router1 = await this.Router.deploy(
-      this.factory1.address,
-      this.weth.address
-    );
-    await this.router1.deployed();
 
-    this.router2 = await this.Router.deploy(
-      this.factory2.address,
-      this.weth.address
-    );
-    await this.router1.deployed();
+    this.factory2 = factory2
 
-    this.buyback = await this.Buyback.deploy(
-      this.router2.address,
-      this.money.address
-    );
-    await this.buyback.deployed();
+    this.router1 = router1
+
+    this.router2 = router2
+
+    this.buyback = buyback
 
     await this.factory1.setBuyback(this.buyback.address);
     await this.factory2.setBuyback(this.buyback.address);
 
-    this.token = await this.ERC20Mock.deploy("TOKEN", "TOKEN", "100000000");
-    await this.token.deployed();
+    this.token = token0
 
     const pair1 = await this.factory1.createPair(
-      this.weth.address,
-      this.token.address
+        this.weth.address,
+        this.token.address
     );
 
     this.lp1 = await this.Core.attach((await pair1.wait()).events[0].args.pair);
 
     const pair2 = await this.factory2.createPair(
-      this.weth.address,
-      this.token.address
+        this.weth.address,
+        this.token.address
     );
 
     this.lp2 = await this.Core.attach((await pair2.wait()).events[0].args.pair);
 
     this.migrator = await this.Migrator.deploy(
-      this.router1.address,
-      this.router2.address
+        this.router1.address,
+        this.router2.address
     );
     await this.migrator.deployed();
+
   });
 
   it("should do the migration successfully", async function () {
+
+    await this.token.connect(this.minter).approve(this.lp1.address, "100000000", { from: this.minter.address });
+
     await this.token
-      .connect(this.minter)
-      .transfer(this.lp1.address, "10000000", { from: this.minter.address });
+        .connect(this.minter)
+        .transfer(this.lp1.address, "10000000", { from: this.minter.address });
 
     await this.weth
-      .connect(this.minter)
-      .transfer(this.lp1.address, "500000", { from: this.minter.address });
+        .connect(this.minter)
+        .transfer(this.lp1.address, "500000", { from: this.minter.address });
 
     await this.lp1.mint(this.minter.address);
     expect(await this.lp1.balanceOf(this.minter.address)).to.equal("2235067");
@@ -95,29 +97,29 @@ describe("Migrator", function () {
 
     // Add some fake revenue
     await this.token
-      .connect(this.minter)
-      .transfer(this.lp1.address, "100000", { from: this.minter.address });
+        .connect(this.minter)
+        .transfer(this.lp1.address, "100000", { from: this.minter.address });
 
     await this.weth
-      .connect(this.minter)
-      .transfer(this.lp1.address, "5000", { from: this.minter.address });
+        .connect(this.minter)
+        .transfer(this.lp1.address, "5000", { from: this.minter.address });
 
     await this.lp1.sync();
 
     await this.lp1
-      .connect(this.minter)
-      .approve(this.migrator.address, lpBalance);
+        .connect(this.minter)
+        .approve(this.migrator.address, lpBalance);
 
     await this.migrator
-      .connect(this.minter)
-      .migrate(
-        this.token.address,
-        this.weth.address,
-        lpBalance,
-        1,
-        1,
-        1000000000000
-      );
+        .connect(this.minter)
+        .migrate(
+            this.token.address,
+            this.weth.address,
+            lpBalance,
+            1,
+            1,
+            1000000000000
+        );
   });
 
   it("should allow first minting from public only after migrator is gone", async function () {
@@ -127,20 +129,20 @@ describe("Migrator", function () {
     await this.tokenx.deployed();
 
     const pair = await this.factory2.createPair(
-      this.weth.address,
-      this.tokenx.address
+        this.weth.address,
+        this.tokenx.address
     );
 
     this.lpx = await this.Core.attach((await pair.wait()).events[0].args.pair);
 
     await this.weth
-      .connect(this.minter)
-      .transfer(this.lpx.address, "10000000", { from: this.minter.address });
+        .connect(this.minter)
+        .transfer(this.lpx.address, "10000000", { from: this.minter.address });
     await this.tokenx
-      .connect(this.minter)
-      .transfer(this.lpx.address, "500000", { from: this.minter.address });
+        .connect(this.minter)
+        .transfer(this.lpx.address, "500000", { from: this.minter.address });
     await expect(this.lpx.mint(this.minter.address)).to.be.revertedWith(
-      "Must not have migrator"
+        "Must not have migrator"
     );
   });
 });
